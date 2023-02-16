7c5363b809ba8004fc3a26778af04238c49f5d70
Stephanie <Stephaniechloe21@gmail.com>
2023-02-16 13:12:33 -0800

    5th task final changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - The main function of the program that print a text
+ * Return: Returns an integer "0"
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
