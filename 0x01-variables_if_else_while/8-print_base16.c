8625efe256599a07b45e7fdb68f3a0c760fc543f
Stephanie <Stephaniechloe21@gmail.com>
2023-02-17 14:04:56 -0800

    9th task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+  * main - A function that prints the letters of the alphabet
+  * Return: Returns an integer value => "0"
+*/
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar('0' + hex[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
