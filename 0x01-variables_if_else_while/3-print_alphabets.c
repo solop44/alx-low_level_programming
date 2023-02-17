c7763c70d2a6cd8540f39bd883e9723a77059615
Stephanie <Stephaniechloe21@gmail.com>
2023-02-17 13:06:11 -0800

    4th task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+  * main - A function that prints the letters of the alphabet
+  * Return: Returns an integer value => "0"
+*/
+int main(void)
+{
+int i = 0;
+while (i < 26)
+{
+putchar(i + 97);
+i++;
+}
+while (i < 26)
+{
+putchar(i + 65);
+i++;
+}
+putchar('\n');
+return (0);
+}
