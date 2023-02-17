e5ed94aaf7ffa59f81a30d6c0ce1748f04a1ed7a
Stephanie <Stephaniechloe21@gmail.com>
2023-02-17 15:32:18 -0800

    11th task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,13 +12,13 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if (i == j || (i > 1 && j < i))
+if (i == j || (i > 0 && j < i))
 {
 continue;
 }
 putchar('0' + i);
 putchar('0' + j);
-if (i == 9 && j == 8)
+if (i == 8 && j == 9)
 {
 break;
 }
