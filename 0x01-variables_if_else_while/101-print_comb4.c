f9d1985d465169f5a1457d6230829fa050eed834
Stephanie <Stephaniechloe21@gmail.com>
2023-02-17 16:20:08 -0800

    Final task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,9 +6,7 @@
 */
 int main(void)
 {
-int i;
-int j;
-int k;
+int i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = 1; j < 9; j++)
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,24 +7,19 @@
 int main(void)
 {
 int i, j;
+putchar('\n');
 for (i = 0; i < 9; i++)
 {
-for (j = 1; j < 10; j++)
+for (j = i + 1; j < 10; j++)
 {
-if (j <= i)
-{
-continue;
-}
 putchar('0' + i);
 putchar('0' + j);
-if (i == 8 && j == 9)
+if (i != 8 || j != 9)
 {
-break;
-}
 putchar(',');
 putchar(' ');
 }
 }
-putchar('\n');
+}
 return (0);
 }
