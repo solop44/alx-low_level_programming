9ce3f61a5a569060b6d6283b00d1dbf923558caa
Stephanie <Stephaniechloe21@gmail.com>
2023-02-23 11:50:33 -0800

    Task 8

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+
 
 /**
  * print_diagonal - prints a diagonal line
@@ -13,12 +13,6 @@ void print_diagonal(int n)
 	int i, j;
 
 	i = 0;
-	if (n <= 0)
-	{
-		_putchar('\n');
-		exit(0);
-	}
-
 	while (i < n)
 	{
 		j = 0;
@@ -31,5 +25,9 @@ void print_diagonal(int n)
 		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+
+	}
 }
