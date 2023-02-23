6cc16e64ccb25549f505b310c49589ff934f93f4
Stephanie <Stephaniechloe21@gmail.com>
2023-02-23 10:37:19 -0800

    10th task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,17 +7,17 @@ void main(void)
 	i = 1;
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+	        else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
