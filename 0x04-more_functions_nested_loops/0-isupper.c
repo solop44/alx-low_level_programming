58ddd16eeed0ea72304e3e1cd5a24b1c47b437dd
Stephanie <Stephaniechloe21@gmail.com>
2023-02-23 07:02:05 -0800

    Added a read me and first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - Checks if characters is uppercase
+ * @c - character
+ * Return: If true 1 else 0
+ */
+
+int _isupper(int c)
+{
+	if(isupper(c) != 0)
+	{
+		return (1);
+
+	}
+	else
+	{
+		return (0);
+	}
+
+}
