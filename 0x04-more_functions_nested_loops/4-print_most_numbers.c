25ce3bead747df7341fac8d0b063aa6c68c9ac52
Stephanie <Stephaniechloe21@gmail.com>
2023-02-23 07:44:13 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers 0 - 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
