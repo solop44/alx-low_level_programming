5f57831b005a56ad45383faefaa97d88c4ee5edc
Stephanie <Stephaniechloe21@gmail.com>
2023-02-23 11:27:01 -0800

    Task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,9 +17,9 @@ void more_numbers(void)
 		{
 			if (j > 9)
 			{
-				putchar(j / 10 + '0');
+				_putchar(j / 10 + '0');
 			}
-			putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
 			j++;
 		}
 		_putchar('\n');
