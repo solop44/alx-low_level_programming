4ef429194d7549cf1c621b11c728a7bdeab7f342
Stephanie <Stephaniechloe21@gmail.com>
2023-02-23 12:22:54 -0800

    Last task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
-
 /**
  * print_number - like a hello world
- *
  * @n: params an integer
- *
- * No return
  */
 
 
 void print_number(int n)
 {
-
 	unsigned int n1 = 0;
 
 	if  (n < 0)
