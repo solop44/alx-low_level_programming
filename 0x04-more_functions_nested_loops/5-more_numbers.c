c1273b71a446c9aeb65a8aefd6937498bef788c5
Stephanie <Stephaniechloe21@gmail.com>
2023-02-23 07:59:04 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints numbers 0 - 14, 10 times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
