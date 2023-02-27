e6203102830e5c37c00c7cea02baff6312c32c40
Stephanie <Stephaniechloe21@gmail.com>
2023-02-27 05:40:12 -0800

    Task 9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - Prints elements from an array
+ * @a: array
+ * @n: number of elements
+ * Return: Void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		if ( i == n - 1)
+		{
+			break;
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+}
