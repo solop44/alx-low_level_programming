d25a4cb2a2df322843ed591a61f7a80accd08c88
Stephanie <Stephaniechloe21@gmail.com>
2023-02-27 03:49:21 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Updates the value of a pointer
+ * @a: pointer value
+ * @b: second pointer value
+ * Return: Void
+*/
+swap_int(int *a, int *b)
+{
+	int val = *a;
+
+	*a = *b;
+	*b = val;
+}
