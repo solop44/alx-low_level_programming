4b5bf95cb59d7800e6a264eac7fcf5271a6790d4
Stephanie <Stephaniechloe21@gmail.com>
2023-02-27 04:19:48 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Prints a reversed string
+ * @s: string
+ * Return: Void
+*/
+void print_rev(char *s)
+{
+	puts(strrev(s));
+}
