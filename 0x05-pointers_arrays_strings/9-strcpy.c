160aade49b95bd7c6e9e6d17b22dfdafb82e7713
Stephanie <Stephaniechloe21@gmail.com>
2023-02-27 06:04:29 -0800

    Task 10

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - Updates the value of a pointer
+ * @dest: destination of string
+ * @src: source of string
+ * Return: character
+*/
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (*dest);
+}
