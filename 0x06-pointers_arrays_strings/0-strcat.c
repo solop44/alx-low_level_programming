7682d18acf4b64392d9c1df6101208257ff49361
Stephanie <Stephaniechloe21@gmail.com>
2023-03-01 01:08:46 -0800

    First task and a read me

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <main.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destination string
+ * @src: second part of string 
+ *
+ * Return: String
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
